d507dd721fe2d8577b6acb708829b9f4d988d0b1
zbagdzevicius <zbagdzevicius@gmail.com>
2019-03-18 16:43:40 +0200

    A

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 setuptools.setup(
      name='convert_csv_to_json',  
-     version='0.1',
-     author="Deepak Kumar",
-     author_email="deepak.kumar.iet@gmail.com",
-     description="A Docker and AWS utility package",
+     version='0.2',
+     author="Zygimantas Bagdzevicius",
+     author_email="zbagdzevicius@gmail.com",
+     description="directly convert csv to json",
      url="https://github.com/zbagdzevicius/convert_csv_to_json",
      packages=setuptools.find_packages(),
      classifiers=[
